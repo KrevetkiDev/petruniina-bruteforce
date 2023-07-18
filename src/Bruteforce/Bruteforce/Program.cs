d4a6ec6c5792bacce88738c8f8934b6399c0fa65
Anastasia <petrunina02@mail.ru>
2023-07-18 18:32:37 +0300

    task done

--- src/Bruteforce/Bruteforce/Program.cs
+++ src/Bruteforce/Bruteforce/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+Stopwatch stopwatch = new Stopwatch();
+Console.Write("Enter your password hash: ");
+string password = Console.ReadLine();
+
+stopwatch.Start();
+var key = Hack(password);
+stopwatch.Stop();
+Console.WriteLine("Your password " + key);
+Console.WriteLine("Time elapsed" + stopwatch.ElapsedMilliseconds);
+
+string Hack(string hash)
+{
+    char[] symbol = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890".ToArray();
+
+    for (int i = 0; i < symbol.Length; i++)
+    {
+        for (int j = 0; j < symbol.Length; j++)
+        {
+            for (int k = 0; k < symbol.Length; k++)
+            {
+                for (int l = 0; l < symbol.Length; l++)
+                {
+                    char[] chars = { symbol[i], symbol[j], symbol[k], symbol[l] };
+                    string txt = new string(chars);
+                    Console.WriteLine(txt);
+                    if (StringSha256Hash(txt) == hash)
+                    {
+                        return txt;
+                    }
+                }
+            }
+        }
+    }
+    return null;
+}
+
+static string StringSha256Hash(string text) =>
+  string.IsNullOrEmpty(text)
+    ? string.Empty
+    : BitConverter
+      .ToString(new SHA256Managed().ComputeHash(
+        Encoding.UTF8.GetBytes(text))).Replace("-", string.Empty);
